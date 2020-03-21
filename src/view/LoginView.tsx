5e0a97aff6abb2a5275be026dbe45954acdd4e48
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-03-21 17:45:36 +0100

    LoginView

---
+++ src/view/LoginView.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SHA256 from "crypto-js/sha256";
+
+const salt = "234lsnfd";
+
+export interface LoginViewProps {
+
+}
+
+export default class LoginView extends React.Component {
+
+    private personalid: String = "";
+
+    constructor(pros:any){
+        super(pros);
+        this.sendLogin = this.sendLogin.bind(this);
+    }
+
+
+    sendLogin(){
+        if (this.personalid.length === 10){
+            console.log(SHA256(salt + this.personalid).toString());
+        }
+    }
+
+    onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.personalid =  e.target.value;
+    };
+
+
+    render() {
+        return (<div><TextField id="filled-basic" label="Personal-ID" variant="filled" onChange={this.onInputChange}/>
+                 <Button variant="contained" onClick={this.sendLogin}>Anmelden</Button></div>);
+    }
+};
