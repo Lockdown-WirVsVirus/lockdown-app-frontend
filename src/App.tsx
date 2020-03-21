c75029ad73b33378c1328a5c47c5fe0422a61654
Arkadius Hauke <notme@hauke.biz>
2020-03-21 15:42:41 +0100

    dependency for reactrouter-dom added, basic views, basic router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import LoginView from "./view/LoginView";
+import CheckView from "./view/CheckView";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>Hello from Lockdown App! 🎸 </p>
-          <p>Develop Branch</p>
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Route path="/" exact component={LoginView}/>
+            <Route path="/login" component={LoginView}/>
+            <Route path="/check" component={CheckView}/>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/view/LoginView.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export interface LoginViewProps {
+
+}
+
+const LoginView = (props: LoginViewProps) => {
+
+    return (
+        <h2>LoginView</h2>
+    );
+};
+
+
+export default LoginView
