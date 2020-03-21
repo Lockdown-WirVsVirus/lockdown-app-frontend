aa4cc6b067f4ee77e080adb1c85ca15aa4faf0f6
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-03-21 15:45:20 +0100

    no rotation

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
       <header className="App-header">
         <img src={Jentelman} className="App-logo" alt="logo" />
         <p>
-        <p>Lockdown App! 🎸 </p>
+        <p>Lockdown App! <span aria-label="guitar" role="img">🎸</span> </p>
         </p>
-        <p>Automated Deployment Pipeline 🚀 <br/>
-           This from develop branch 👨🏼‍💻
+        <p>Automated Deployment Pipeline <span aria-label="rocket" role="img">🚀</span> <br/>
+           This from develop branch <span aria-label="developer" role="img">👨🏼‍💻</span>
         </p>
       </header>
     </div>
