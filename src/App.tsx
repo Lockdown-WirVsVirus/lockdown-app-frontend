ca2190f562e9dbace1d5c31882e5da884c8750c3
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-03-21 15:25:44 +0100

    test develop pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p>Hello from Lockdown App! 🎸 </p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>Hello from Lockdown App! 🎸 </p>
-          <p>Develop Branch</p>
-        </a>
+        <p>Automated Develop Branch</p>
       </header>
     </div>
   );
