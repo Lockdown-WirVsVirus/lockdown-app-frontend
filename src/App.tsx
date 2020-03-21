7f73b4e7eb58b180649647767bd420887721e1ce
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-03-21 14:31:32 +0100

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello from Lockdown App! 🎸
         </a>
       </header>
     </div>
