360c3d68b998a95dc0b71ea158f76b734450b996
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-03-22 17:43:57 +0100

    home and ticket view with stored ticket from LS

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import DownloadPdfView from "./view/DownloadPdfView";
 import ShowLsPdfView from "./view/ShowLsPdfView";
 import RenderLsPdfView from "./view/RenderLsPdfView";
 import LeaveRequestView from "./view/LeaveRequestView";
+import TicketView from "./view/TicketView";
+import HomeView from "./view/HomeView";
 
 import moment from "moment";
 import "moment/locale/de";
@@ -23,6 +25,8 @@ function App() {
         <Route path="/open" component={ShowLsPdfView} />
         <Route path="/render" component={RenderLsPdfView} />
         <Route path="/leave" component={LeaveRequestView} />
+        <Route path="/ticket" component={TicketView} />
+        <Route path="/home" component={HomeView} />
       </Router>
   );
 }
