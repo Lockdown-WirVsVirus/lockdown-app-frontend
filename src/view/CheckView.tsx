fb666b03b04016fedd7bd34448dc504598aa6197
twieprich <wieprich@appplant.de>
2020-03-22 16:59:45 +0100

    Update

--- src/view/CheckView.tsx
+++ src/view/CheckView.tsx
@@ -2,19 +2,38 @@ import React, {useEffect} from "react"
 import Cookies from 'universal-cookie';
 import {TicketPayload, Address} from "../gen-backend-api/api";
 import TicketFacade from "../service/TicketFacade";
-import { Document } from 'react-pdf';
+import axios from 'axios';
+import { Document, pdfjs, Page } from "react-pdf";
 import Header from "./Components/Header";
-
-
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${ pdfjs.version }/pdf.worker.js`
 export interface CheckViewProperties {
 
 }
 
 const CheckView = (props: CheckViewProperties) => {
     const cookies = new Cookies();
-    console.log(cookies.get("hashPersonalID"))
+    
+    const [requestReason, setRequestReason] = React.useState<Blob | null>();
 
+
+
+    const dataURItoBlob = (dataURI: any) => {
+        var mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        var binary = atob(dataURI.split(',')[1]);
+        var array = [];
+        for (var i = 0; i < binary.length; i++) {
+            array.push(binary.charCodeAt(i));
+        }
+        return new Blob([new Uint8Array(array)], {type: mime});
+    }
+
+
+    const getNameById  = () => {
+        return axios.get("./pdf/Test1.pdf")
+            .then(response => {
+              return setRequestReason( new Blob(response.data))
+            })
+        }
     useEffect(() => {
         const payload: TicketPayload = {
             arrivalTime: new Date().getTime(),
@@ -33,7 +52,13 @@ const CheckView = (props: CheckViewProperties) => {
         });
     }, [])
     return (
-        <Header title="Check"></Header>
+        <div>
+        <Document file="/pdf/Test1.pdf"  onSourceSuccess={() => {
+                console.log("Source success")
+              }}><Page pageNumber={1}
+              height={600}
+              className="PDFPage PDFPageOne"/></Document>
+      </div>
     );
 };
 
