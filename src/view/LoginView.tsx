9d2c3fdc2e0135271a8d2554d36da6807db06dd2
Arkadius Hauke <riskylemur@hauke.biz>
2020-03-22 12:40:04 +0100

    initial commit for TicketFacade

--- src/view/LoginView.tsx
+++ src/view/LoginView.tsx
@@ -5,100 +5,102 @@ import SHA256 from "crypto-js/sha256";
 import Cookies from 'universal-cookie';
 
 import IdentityProvider from "../service/identityProvider";
+import WindowConfig from "../service/WindowConfig";
 
 const salt = "234lsnfd";
 
-interface LoginProps {}
+interface LoginProps {
+}
 
 interface LoginState {
-  personalid: string;
-  firstName: string;
-  lastName: string;
-  disable: boolean;
+    personalid: string;
+    firstName: string;
+    lastName: string;
+    disable: boolean;
 }
 
 export default class LoginView extends React.Component<LoginProps, LoginState> {
 
-  constructor(pros: any) {
-    super(pros);
-    this.state = {
-      personalid: "",
-      firstName: "",
-      lastName: "",
-      disable: false
-    };
-    this.sendLogin = this.sendLogin.bind(this);
-  }
+    constructor(pros: any) {
+        super(pros);
+        this.state = {
+            personalid: "",
+            firstName: "",
+            lastName: "",
+            disable: false
+        };
+        this.sendLogin = this.sendLogin.bind(this);
+    }
 
-  sendLogin(){
-    console.log(SHA256(salt + this.state.personalid).toString());
-    const cookies = new Cookies();
-    cookies.set('firstname', this.state.firstName);
-    cookies.set('lastname', this.state.lastName);
-    cookies.set('personalid', this.state.personalid);
-    cookies.set('hashPersonalID', SHA256(salt + this.state.personalid).toString());
+    sendLogin() {
+        console.log(SHA256(salt + this.state.personalid).toString());
+        const cookies = new Cookies();
+        cookies.set('firstname', this.state.firstName);
+        cookies.set('lastname', this.state.lastName);
+        cookies.set('personalid', this.state.personalid);
+        cookies.set('hashPersonalID', SHA256(salt + this.state.personalid).toString());
 
-    IdentityProvider.setIdentity(this.state.firstName, this.state.lastName, this.state.personalid);
-  }
+        IdentityProvider.setIdentity(this.state.firstName, this.state.lastName, this.state.personalid);
+    }
 
-  validInput(firstname: String, lastname: String, personalid: String) {
-    // let result = !(
-    //   firstname.length > 0 &&
-    //   lastname.length > 0 &&
-    //   personalid.length == 10
-    // );
-    // this.setState({ disable: result });
-  }
+    validInput(firstname: String, lastname: String, personalid: String) {
+        // let result = !(
+        //   firstname.length > 0 &&
+        //   lastname.length > 0 &&
+        //   personalid.length == 10
+        // );
+        // this.setState({ disable: result });
+    }
 
-  render() {
-    return (
-      <div className="LoginView">
-        <TextField
-          id="filled-basic"
-          label="Vorname"
-          variant="filled"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            this.setState({ firstName: e.target.value });
-            this.validInput(
-              e.target.value,
-              this.state.lastName,
-              this.state.personalid
-            );
-          }}
-        />
-        <br />
-        <TextField
-          id="filled-basic"
-          label="Nachname"
-          variant="filled"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            this.setState({ lastName: e.target.value });
-            this.validInput(
-              this.state.firstName,
-              e.target.value,
-              this.state.personalid
-            );
-          }}
-        />
-        <br />
-        <TextField
-          id="filled-basic"
-          label="Personal-ID"
-          variant="filled"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            this.setState({ personalid: e.target.value });
-            this.validInput(
-              this.state.firstName,
-              this.state.lastName,
-              e.target.value
-            );
-          }}
-        />
-        <br />
-        <Button variant="contained" disabled={this.state.disable} href={"leave"} onClick={this.sendLogin}>
-          Anmelden
-        </Button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="LoginView">
+                <TextField
+                    id="filled-basic"
+                    label="Vorname"
+                    variant="filled"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        this.setState({firstName: e.target.value});
+                        this.validInput(
+                            e.target.value,
+                            this.state.lastName,
+                            this.state.personalid
+                        );
+                    }}
+                />
+                <br/>
+                <TextField
+                    id="filled-basic"
+                    label="Nachname"
+                    variant="filled"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        this.setState({lastName: e.target.value});
+                        this.validInput(
+                            this.state.firstName,
+                            e.target.value,
+                            this.state.personalid
+                        );
+                    }}
+                />
+                <br/>
+                <TextField
+                    id="filled-basic"
+                    label="Personal-ID"
+                    variant="filled"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        this.setState({personalid: e.target.value});
+                        this.validInput(
+                            this.state.firstName,
+                            this.state.lastName,
+                            e.target.value
+                        );
+                    }}
+                />
+                <br/>
+                <Button variant="contained" disabled={this.state.disable} href={"leave"} onClick={this.sendLogin}>
+                    Anmelden
+                </Button>
+            </div>
+        );
+    }
 }
