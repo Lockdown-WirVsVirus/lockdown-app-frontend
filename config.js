3d481277c9c0a146f3c00605e72904bb46343512
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-03-29 20:54:19 +0200

    fix backend config

--- config.js
+++
@@ -1,2 +0,0 @@
-// this is dummy file for dev purposescoop install insomnia
-window.BACKEND_URL = 'https://dev-lockdown-app-backend-hesbetn2tq-ew.a.run.app/'
---
+++ public/config.js
@@ -0,0 +1,2 @@
+// this is dummy file for dev purpose
+window.BACKEND_URL = 'https://dev-lockdown-app-backend-hesbetn2tq-ew.a.run.app/api'
