9407afdf6c7ee7b343256d976018937a4124ab5f
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-04-13 18:18:38 +0200

    just dirty fix of test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Lockdown/i);
+  const linkElement = getByText(/Anmeldung/i);
   expect(linkElement).toBeInTheDocument();
 });
