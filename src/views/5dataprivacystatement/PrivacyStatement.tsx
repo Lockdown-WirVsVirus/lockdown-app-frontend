7fd3f64370e12be8b410b7a46bf0f863400dc26f
Iranox <tgwieprich@gmail.com>
2020-06-28 14:20:51 +0200

    new page

---
+++ src/views/5dataprivacystatement/PrivacyStatement.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import Header from "../../components/Header";
+
+
+
+const PrivacyStatementView = () => {
+    return (
+        <div>
+            <Header title="Datenschutzhinweis"></Header>
+        </div>
+    );
+}
